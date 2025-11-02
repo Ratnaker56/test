45901b3a597dc7f73b384a68571fcb54eb8137c0
Ratnaker56 <138117387+Ratnaker56@users.noreply.github.com>
2025-11-03 01:51:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker! This is v2.'
+    return 'Hello, Docker! This pipeline works!'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
