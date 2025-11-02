c2d5f631645e0eca058a7645c0548e483f725da2
Ratnaker56 <138117387+Ratnaker56@users.noreply.github.com>
2025-11-03 02:15:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker! This pipeline works!'
+    return 'Hello, Docker! This is v2 and my CI/CD pipeline is complete!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
